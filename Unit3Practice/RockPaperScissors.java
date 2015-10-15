4591affbb29460795926e032c9e12fef9ddea1fa
Hobbs2000 <cjfield@NN121-HA-15.HS.sd203.org>
2015-10-15 10:30:34 -0500

    Started a practice prompt

---
+++ Unit3Practice/RockPaperScissors.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RockPaperScissors
+{
+     public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Player 1: Choose rock, scissors, or paper:");
+        String player1 = scan.next().toLowerCase();
+        System.out.println("Player 2: Choose rock, scissors, or paper:");
+        String player2 = scan.next().toLowerCase(); 
+        
+        if (player1.equals(player2))
+        {
+            System.out.println("Its a tie!");
+        }
+        else if (player1.equals("rock") && player2.equals("scissors") || player1.equals("scissors") && player2.equals("paper") || player1.equals("paper") && player2.equals("rock")))
+        {
+            System.out.println("Player 1 wins!");
+        }
+        else if (player2.equals("rock") && player1.equals("scissors"))
+        {
+            System.out.println("Player 2 wins!");
+        }
+        
+    }
+}
